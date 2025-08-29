1078489a38f24bcb621029bdcc3a7b007ad3b0cc
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-29 22:46:07 +0300

    feet: add assignment 3 of day 5

---
+++ ITI Belts - First Round/Day-05/ConsoleApp3/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ ITI Belts - First Round/Day-05/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine($"Sum of 2 integers: {calc.Add(5, 10)}");
+            Console.WriteLine($"Sum of 2 doubles: {calc.Add(3.5, 6.5)}");
+            Console.WriteLine($"Sum of 3 integers: {calc.Add(1, 2, 3)}");
+        }
+    }
+}
