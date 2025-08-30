1974790ea63ef0766fd50e84ba2ddaa2d02f57c9
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-30 12:14:31 +0300

    feet: add assignment 4 of day 5

---
+++ ITI Belts - First Round/Day-05/ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Zeyad");
+            Console.WriteLine("Student 1 Info:");
+            Console.WriteLine($"Name: {s1.Name}, Age: {s1.Age}");
+
+            Student s2 = new Student("Waled", 20);
+            Console.WriteLine("Student 2 Info:");
+            Console.WriteLine($"Name: {s2.Name}, Age: {s2.Age}");
+        }
+    }
+}
---
+++ ITI Belts - First Round/Day-05/ConsoleApp4/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+            Age = 0;
+        }
+
+        public Student(string name, int age) : this(name)
+        {
+            Age = age;
+        }
+    }
+}
