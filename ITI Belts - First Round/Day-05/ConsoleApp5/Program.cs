f30938fce70a690018b2225139e8d49069bb08b6
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-08-30 12:14:46 +0300

    feet: add assignment 5 of day 5

---
+++ ITI Belts - First Round/Day-05/ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(3, 4);
+            Vector v2 = new Vector(1, 2);
+
+            Console.WriteLine($"v1: {v1}");
+            Console.WriteLine($"v2: {v2}");
+            Console.WriteLine($"v1 + v2: {v1 + v2}");
+            Console.WriteLine($"v1 - v2: {v1 - v2}");
+            Console.WriteLine($"v1 == v2: {v1 == v2}");
+            Console.WriteLine($"v1 != v2: {v1 != v2}");
+        }
+    }
+}
---
+++ ITI Belts - First Round/Day-05/ConsoleApp5/Vector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Vector
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static bool operator ==(Vector v1, Vector v2)
+        {
+            return v1.X == v2.X && v1.Y == v2.Y;
+        }
+        public static bool operator !=(Vector v1, Vector v2)
+        {
+            return v1.X != v2.X && v1.Y != v2.Y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
