eb43b8922e82ccae20884b4c2575b36da0049564
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-04 17:19:26 +0300

    feet: add assignment 1 of day 1 in advanced

---
+++ ITI Belts - First Round/Advanced C#/Day-01/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>
+            {
+                new Student("Zeyad", 20),
+                new Student("Waled", 22),
+                new Student("Ebrahim", null)
+            };
+
+            students[0].AddScores(85, 90, 95);
+            students[1].AddScores(70, 65, 80);
+            students[2].AddScores(75, 80, 80);
+
+            foreach (var student in students)
+            {
+                var studentInfo = new
+                {
+                    student.Name,
+                    student.Age,
+                    AverageScore = student.GetAverageScore()
+                };
+
+                Console.WriteLine($"Name: {studentInfo.Name}, Age: {studentInfo.Age}, Average Score: {studentInfo.AverageScore}");
+            }
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-01/ConsoleApp1/Student.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public List<int> Scores { get; set; }
+
+        public Student(string name, int? age = null)
+        {
+            Name = name;
+            Age = age;
+            Scores = new List<int>();
+        }
+
+        public void AddScores(params int[] scores)
+        {
+            foreach (var score in scores)
+            {
+                Scores.Add(score);
+            }
+        }
+
+        public double? GetAverageScore()
+        {
+            if (Scores.Count == 0) return null;
+            return Scores.Average();
+        }
+    }
+}
