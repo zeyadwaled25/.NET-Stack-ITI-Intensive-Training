1b329d5fb79f6b2bf2067bf3c4b87f5d30b2fee1
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-06 11:24:33 +0300

    feet: add assignment 3 of day 1 in advanced

---
+++ ITI Belts - First Round/Advanced C#/Day-01/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿    namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] mixedData = { 10, "20", "hello", 30, "40", "world", 50 };
+
+            var numbers = new List<int>();
+            var strings = new List<string>();
+
+            foreach (dynamic item in mixedData)
+            {
+                if (item is string str && int.TryParse(str, out int number))
+                {
+                    numbers.Add(number);
+                }
+                else if (item is string)
+                {
+                    strings.Add(item);
+                }
+                else if (item is int)
+                {
+                    numbers.Add(item);
+                }
+            }
+
+            int totalSum = numbers.Sum();
+
+            Console.WriteLine("Original Data: " + string.Join(", ", mixedData));
+            Console.WriteLine("All Strings: " + string.Join(", ", strings));
+            Console.WriteLine("All Numbers: " + string.Join(", ", numbers));
+            Console.WriteLine("Sum of Numbers: " + totalSum);
+        }
+    }
+}
