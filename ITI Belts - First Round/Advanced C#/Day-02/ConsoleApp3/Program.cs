5e81abfb7f480f0f899fa9cd32b8cbf777ed7ead
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-08 00:05:27 +0300

    feet: add assignment 3 of day 2 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-02/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>
+            {
+                "Ali", "Sara", "Mona", "Abdullah", "Mohammed"
+            };
+            students.Add("Laila");
+            students.Add("Omar");
+            students.Add("Ali");
+
+            students.Remove("Mona");
+
+            Console.WriteLine("All Students:");
+            foreach (var s in students)
+                Console.Write(s + ", ");
+            Console.WriteLine();
+
+            Console.WriteLine("\nNames starting with 'A':");
+            foreach (var s in students.Where(x => x.StartsWith("A")))
+                Console.Write(s + ", ");
+        }
+    }
+}
