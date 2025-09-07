32d04b9c8298a98f08363c56ea43fbe5fce5b04b
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-08 00:05:06 +0300

    feet: add assignment 2 of day 2 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-02/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 15, 20 };
+
+            void Calculate()
+            {
+                int GetSum()
+                {
+                    int sum = 0;
+                    foreach (var num in numbers)
+                        sum += num;
+                    return sum;
+                }
+                int sum = GetSum();
+                double avg = (double)sum / numbers.Length;
+
+                Console.WriteLine($"Sum = {sum}");
+                Console.WriteLine($"Average = {avg}");
+            }
+            Calculate();
+        }
+    }
+}
