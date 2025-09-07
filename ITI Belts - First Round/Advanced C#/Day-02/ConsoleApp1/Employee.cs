32c3bdf65744f147eda510f0e7c6ed0425477335
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-08 00:04:44 +0300

    feet: add assignment 1 of day 2 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-02/ConsoleApp1/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-02/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void CheckEmployeeRole(Employee emp)
+        {
+            if (emp is Manager)
+                Console.WriteLine($"{emp.Name} (Manager): Approve Budget & Conduct Meetings");
+            else if (emp is Admin)
+                Console.WriteLine($"{emp.Name} (Admin): Manage Users & Handle Permissions");
+            else if (emp is Staff)
+                Console.WriteLine($"{emp.Name} (Staff): Perform Daily Tasks");
+            else
+                Console.WriteLine("Unknown Role");
+        }
+        static void Main(string[] args)
+        {
+            Employee[] employees = {
+                new Manager("Zeyad", 10000),
+                new Admin("Waled", 8000),
+                new Staff("Ebrahim", 5000),
+            };
+
+            foreach (var emp in employees)
+            {
+                CheckEmployeeRole(emp);
+            }
+        }
+    }
+}
