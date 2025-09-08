15d98c5010f811262456fd949ee3a2e42d5fe187
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-08 12:16:05 +0300

    feet: add assignment 5 of day 2 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-02/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void ModifyArray(int[] arr)
+        {
+            arr[0] = 100;
+            arr = new int[] { 9, 9, 9 };
+        }
+
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3 };
+
+            Console.WriteLine("Before:");
+            Console.WriteLine(string.Join(", ", arr));
+
+            ModifyArray(arr);
+
+            Console.WriteLine("After:");
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
