bd5a72ab2da306fa45414c86b7850efdd297d397
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-08 12:15:50 +0300

    feet: add assignment 4 of day 2 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-02/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> countries = new Dictionary<string, string>
+            {
+                { "EG", "Egypt" },
+                { "US", "United States" },
+                { "UK", "United Kingdom" }
+            };
+
+            Console.Write("Enter country code (EG, US, UK): ");
+            string code = Console.ReadLine().ToUpper();
+
+            if (countries.ContainsKey(code))
+                Console.WriteLine($"Country: {countries[code]}");
+            else
+                Console.WriteLine("Code Not Found");
+        }
+    }
+}
