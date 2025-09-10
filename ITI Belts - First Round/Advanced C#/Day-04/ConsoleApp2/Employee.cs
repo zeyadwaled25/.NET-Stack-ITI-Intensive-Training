1b4d773645438e3e0ee8fa4162964891d728dfa7
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-10 10:18:00 +0300

    feet: add task 2 of assignment for day 4 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-04/ConsoleApp2/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Employee : IComparable<Employee>
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public DateTime HiringDate { get; set; }
+
+        //compereto using year then month then day
+        public int CompareTo(Employee other)
+        {
+            int yearCompare = this.HiringDate.Year.CompareTo(other.HiringDate.Year);
+            if (yearCompare != 0)
+                return yearCompare;
+
+            int monthCompare = this.HiringDate.Month.CompareTo(other.HiringDate.Month);
+            if (monthCompare != 0)
+                return monthCompare;
+
+            return this.HiringDate.Day.CompareTo(other.HiringDate.Day);
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-04/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>
+        {
+            new Employee { Name = "Zeyad", Salary = 70000, HiringDate = new DateTime(2024, 3, 15) },
+            new Employee { Name = "Waled", Salary = 80000, HiringDate = new DateTime(2023, 8, 1) },
+            new Employee { Name = "Ebrahim", Salary = 60000, HiringDate = new DateTime(2025, 1, 30) }
+        };
+
+            employees.Sort();
+
+            foreach (var e in employees)
+            {
+                Console.WriteLine($"{e.Name}: {e.Salary}, {e.HiringDate.ToShortDateString()}");
+            }
+        }
+    }
+}
