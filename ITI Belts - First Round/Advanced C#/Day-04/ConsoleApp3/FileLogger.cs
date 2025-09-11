d9d101f6cfe2baf2932dcbff9644c94dca56d27e
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-11 09:09:13 +0300

    feet: add task 3 of assignment for day 4 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-04/ConsoleApp3/FileLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class FileLogger : IDisposable
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = new StreamWriter(path, append: true);
+        }
+
+        public void LogMessage(string message)
+        {
+            writer.WriteLine($"{DateTime.Now}: {message}");
+        }
+
+        public void Dispose()
+        {
+            writer?.Close();
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-04/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var logger = new FileLogger("log.txt"))
+            {
+                logger.LogMessage("First message");
+                logger.LogMessage("Second message");
+                logger.LogMessage("Third message");
+            }
+        }
+    }
+}
