a57b1435ba2ba6a29ff1d9e09b1d0315321d5e5d
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-13 10:07:11 +0300

    feet: complete tasks 5 and 6 of assignment for day 5 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5
+{
+    public delegate bool NumberCheck(int number);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberCheck isPositive = n => n > 0;
+
+            int[] testNumbers = { -1, 0, 1 };
+            foreach (var num in testNumbers)
+            {
+                Console.WriteLine($"{num} is Positive? {isPositive(num)}");
+            }
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string, int> printMultiple = (msg, count) =>
+            {
+                for (int i = 0; i < count; i++)
+                    Console.WriteLine(msg);
+            };
+
+            printMultiple("Hello", 3);
+        }
+    }
+}
