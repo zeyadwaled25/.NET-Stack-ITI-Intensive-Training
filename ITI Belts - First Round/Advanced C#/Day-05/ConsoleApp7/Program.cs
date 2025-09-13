d9bccd88599fbda9731cc96eb4884cb5bec2f904
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-13 10:07:29 +0300

    feet: complete tasks 7 and 8 of assignment for day 5 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double, double> add = (a, b) => a + b;
+            Func<double, double, double> average = (a, b) => (a + b) / 2;
+
+            double a = 4.5, b = 7.5;
+
+            Console.WriteLine($"Add = {add(a, b)}");
+            Console.WriteLine($"Average = {average(a, b)}");
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> startsWithA = s => s.StartsWith("A");
+
+            string[] items = { "Apple", "Banana", "Avocado", "Mango" };
+            foreach (string item in items)
+            {
+                if (startsWithA(item))
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
