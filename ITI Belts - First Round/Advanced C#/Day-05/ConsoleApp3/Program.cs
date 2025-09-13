0445069ce5d69ab3f1324822bc583447171f07dc
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-13 10:06:12 +0300

    feet: complete tasks 3 and 4 of assignment for day 5 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3
+{
+    public delegate double DiscountCalculator(double price);
+    internal class Program
+    {
+        static double TenPercentOff(double price) => price - (0.10 * price);
+        static double FiftyOff(double price) => price - 50;
+
+        static void ShowFinalPrice(double price, DiscountCalculator dc)
+        {
+            Console.WriteLine($"Original Price: {price}");
+            double finalPrice = dc(price);
+            Console.WriteLine($"Final Price: {finalPrice}");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            ShowFinalPrice(200, TenPercentOff);
+            ShowFinalPrice(200, FiftyOff);
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp4
+{
+    public delegate void Notifier();
+    internal class Program
+    {
+        static void SendSMS() => Console.WriteLine("SMS sent");
+        static void SendEmail() => Console.WriteLine("Email sent");
+        static void ShowPopup() => Console.WriteLine("Popup notification shown");
+        static void Main(string[] args)
+        {
+            Notifier notify = SendSMS;
+            notify += SendEmail;
+            notify += ShowPopup;
+
+            notify();
+        }
+    }
+}
