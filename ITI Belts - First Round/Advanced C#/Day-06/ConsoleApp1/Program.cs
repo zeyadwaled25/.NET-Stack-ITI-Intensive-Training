8fa0db32fc111e5b624ece063af348a6d8c5492a
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-14 00:24:27 +0300

    feet: add task 1 of assignment for day 6 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void StopPump()
+        {
+            Console.WriteLine("Pump stopped, tank is full!");
+        }
+        static void Main(string[] args)
+        {
+            WaterTank tank = new WaterTank();
+            tank.TankFull += StopPump;
+            tank.Fill(50);
+            tank.Fill(50);
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp1/WaterTank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate void TankFullHandler();
+    class WaterTank
+    {
+        public event TankFullHandler TankFull;
+
+        private int level = 0;
+        public void Fill(int amount)
+        {
+            level += amount;
+            if (level >= 100)
+            {
+                TankFull?.Invoke();
+            }
+        }
+    }
+}
