5b8714ceb816c7d8ef1492cc1976704bb6b9abca
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-13 10:05:55 +0300

    feet: add task 1 and 2 of assignment for day 5 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    public delegate void WelcomeMessage(string name);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage welcome = (string name) => Console.WriteLine($"Welcome, {name}!");
+            welcome("Mohamed");
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-05/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    public delegate int MathOperation(int a, int b);
+    internal class Program
+    {
+        static int Add(int a, int b) => a + b;
+        static int Subtract(int a, int b) => a - b;
+        static int Multiply(int a, int b) => a * b;
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            MathOperation add = Add;
+            Console.WriteLine($"Add = {add(x, y)}");
+
+            MathOperation subtract = Subtract;
+            Console.WriteLine($"Subtract = {subtract(x, y)}");
+
+            MathOperation multiply = Multiply;
+            Console.WriteLine($"Multiply = {multiply(x, y)}");
+        }
+    }
+}
