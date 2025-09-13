3a8d1ad290593d45ccc2390af9f79babdb710976
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-14 00:24:42 +0300

    feet: add task 2 of assignment for day 6 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp2/LightSensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public delegate void LightChangedHandler(int luxLevel);
+    class LightSensor
+    {
+        public event LightChangedHandler LightChanged;
+
+        public void SetLightLevel(int level)
+        {
+            LightChanged?.Invoke(level);
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void TurnOnLamp(int luxLevel)
+        {
+            Console.WriteLine($"Lamp turned on, lux: {luxLevel}");
+        }
+        static void LogLightChange(int luxLevel)
+        {
+            Console.WriteLine($"Light level changed to {luxLevel}");
+        }
+        static void Main(string[] args)
+        {
+            LightSensor sensor = new LightSensor();
+            sensor.LightChanged += TurnOnLamp;
+            sensor.LightChanged += LogLightChange;
+
+            sensor.SetLightLevel(300);
+        }
+    }
+}
