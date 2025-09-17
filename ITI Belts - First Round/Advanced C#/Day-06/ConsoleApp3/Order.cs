097ade96eb7bbffd465d237504dc0c46948edbb2
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-17 09:30:37 +0300

    feet: add task 3 of assignment for day 6 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp3/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Order
+    {
+        public event EventHandler OrderPlaced;
+
+        public void PlaceOrder()
+        {
+            OrderPlaced?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void SendConfirmation(object sender, EventArgs e)
+        {
+            Console.WriteLine("Order placed successfully!");
+        }
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.OrderPlaced += SendConfirmation;
+
+            order.PlaceOrder();
+        }
+    }
+}
