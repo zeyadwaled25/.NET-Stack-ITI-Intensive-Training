a43ab8ddc651e56c171cae8f8791abf85ce94ccb
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-17 09:31:05 +0300

    feet: add task 6 of assignment for day 6 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static async Task ReadFileAsync()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("File read complete");
+        }
+
+        static async Task AnalyzeFileAsync()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("File analysis complete");
+        }
+
+        static void Main()
+        {
+            var readTask = ReadFileAsync();
+            var analyzeTask = AnalyzeFileAsync();
+            readTask.Wait();
+            analyzeTask.Wait();
+        }
+    }
+}
