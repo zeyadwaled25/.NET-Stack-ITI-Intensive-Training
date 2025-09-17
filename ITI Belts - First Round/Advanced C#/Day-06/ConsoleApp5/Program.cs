b98d6da32d95ca8f077c4cb56c7e9cbe68b90a38
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-09-17 09:30:56 +0300

    feet: add task 5 of assignment for day 6 in advanced C#

---
+++ ITI Belts - First Round/Advanced C#/Day-06/ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static async Task BrewCoffeeAsync()
+        {
+            await Task.Delay(3000);
+            Console.WriteLine("Coffee is ready!");
+        }
+
+        static async Task Main()
+        {
+            await BrewCoffeeAsync();
+        }
+    }
+}
